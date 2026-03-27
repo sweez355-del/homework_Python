4849b6d979821e12ed8f0b17f537f2fb021716dc
sweez355-del <sweez355@gmail.com>
2026-03-27 16:50:13 +0300

    Fix_lesson05_task3.py

--- lesson05_task3.py
+++ lesson05_task3.py
@@ -3,16 +3,25 @@ from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.common.by import By
+
+
 driver = webdriver.Firefox(
     service=FirefoxService(GeckoDriverManager().install())
 )
+
 driver.maximize_window()
+
 driver.get("http://the-internet.herokuapp.com/inputs")
+
 search_input = driver.find_element(By.CSS_SELECTOR, '[type="number"]')
-search_input.send_keys('Sky')
+
+search_input.send_keys('12345')
 sleep(1)
+
 search_input.clear()
 sleep(1)
-search_input.send_keys('Pro')
+
+search_input.send_keys('54321')
 sleep(1)
+
 driver.quit()
