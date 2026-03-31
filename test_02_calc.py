44a2b814a463c78bf0c8a06ba0d4083b05b50b86
sweez355-del <sweez355@gmail.com>
2026-03-31 22:05:45 +0300

    Update test_02_calc.py

--- test_02_calc.py
+++ test_02_calc.py
@@ -1,38 +1,28 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
-# Регистрируем драйвер для Chrome
-driver = webdriver.Chrome()
-driver.maximize_window()
-
-# Заходим на страницу
-driver.get(
-    "https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html"
-)
-waiter = WebDriverWait(driver, 45)
-
-# Ищем элементы и взаимодействуем с ним
-delay_form = driver.find_element(By.CSS_SELECTOR, "#delay")
-delay_form.clear()
-delay_form.send_keys("45")
-
-driver.find_element(By.XPATH, "//span[text()='7']").click()
-driver.find_element(By.XPATH, "//span[text()='+']").click()
-driver.find_element(By.XPATH, "//span[text()='8']").click()
-driver.find_element(By.XPATH, "//span[text()='=']").click()
-
-waiter.until(
-    EC.text_to_be_present_in_element((By.CSS_SELECTOR, ".screen"), "15")
-)
-result = driver.find_element(By.CSS_SELECTOR, ".screen").text
+from selenium.webdriver.support.wait import WebDriverWait
 
 
-# Закрываем драйвер
-driver.quit()
+def click(driver, b):
+    driver.find_element(By.XPATH, f'//span[text()="{b}"]').click()
 
 
-def test_calc_result():
-    """Проверка корректного ответа калькулятора"""
+def test_calculator():
+    driver = webdriver.Chrome()
+    driver.get(
+        "https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
+    input_field = driver.find_element(By.CSS_SELECTOR, "#delay")
+    input_field.clear()
+    input_field.send_keys("45")
+    buttons = ["7", "+", "8", "="]
+
+    for btn in buttons:
+        click(driver, btn)
+    wait = WebDriverWait(driver, 46)
+    wait.until(
+        EC.text_to_be_present_in_element((By.CSS_SELECTOR, ".screen"), "15"))
+    result = driver.find_element(By.CSS_SELECTOR, ".screen").text
     assert result == "15"
+    print(f'Результат:{result}')
+    driver.quit()
